57ea57e70d11edb510830859a75fa5b2323983ed
madiocre <wolvirex@gmail.com>
2024-07-22 00:01:33 +0300

    attempted fix

--- 0-simple_helper_function.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-""" Tuple Helper Function """
-
-
-def index_range(page: int, page_size: int) -> tuple[int, int]:
-    return (page * page_size - page_size), page * page_size
