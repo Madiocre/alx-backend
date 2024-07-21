edb1611d7bea41eeff20eda8d12a53ce080d9888
madiocre <wolvirex@gmail.com>
2024-07-22 00:17:55 +0300

    simple + hypermedia fix style

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 """Task 1: Simple pagination.
 """
-
 import csv
 import math
 from typing import List, Tuple
@@ -41,3 +40,18 @@ class Server:
         if start > len(data):
             return []
         return data[start:end]
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
+        """Retrieves information about a page.
+        """
+        data = self.get_page(page, page_size)
+        start, end = index_range(page, page_size)
+        total_pages = math.ceil(len(self.__dataset) / page_size)
+        return {
+            'page_size': len(data),
+            'page': page,
+            'data': data,
+            'next_page': page + 1 if end < len(self.__dataset) else None,
+            'prev_page': page - 1 if start > 0 else None,
+            'total_pages': total_pages
+        }
