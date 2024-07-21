f2fbdc21300df46808f6bfb4d291f720b2799b06
madiocre <wolvirex@gmail.com>
2024-07-21 23:50:43 +0300

    pagination fix

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -3,4 +3,4 @@
 
 
 def index_range(page: int, page_size: int) -> tuple[int, int]:
-    return (page * page_size - page_size), page * page_size
+    return ((page - 1) * page_size), ((page - 1) * page_size + page_size)
