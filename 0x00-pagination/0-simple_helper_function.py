f31ed49284a4f0d614e445e7d27a966e5d5ee935
madiocre <wolvirex@gmail.com>
2024-07-21 23:54:51 +0300

    pagination fix

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 """ Tuple Helper Function """
+from typing import Tuple
 
 
-def index_range(page: int, page_size: int) -> tuple[int, int]:
-    return ((page - 1) * page_size), ((page - 1) * page_size + page_size)
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """ Calculates start index and an end index corresponding to the range """
+    
+    return (page * page_size - page_size), (page * page_size)
