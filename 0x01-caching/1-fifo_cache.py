97c49c9f81bb8fe2391832ec519b7013fa0b6ee3
madiocre <wolvirex@gmail.com>
2024-07-24 17:17:14 +0300

    LIFO & FIFO caching + Style

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -21,10 +21,10 @@ class FIFOCache(BaseCaching):
         """
         if key is None or item is None:
             return
-
-        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            first_key, _ = self.cache_data.popitem(last=False)
-            print(f"DISCARD: {first_key}")
+        if key not in self.cache_data:
+            if len(self.cache_data) == BaseCaching.MAX_ITEMS:
+                first_key, _ = self.cache_data.popitem(last=False)
+                print(f"DISCARD: {first_key}")
 
         self.cache_data[key] = item
 
