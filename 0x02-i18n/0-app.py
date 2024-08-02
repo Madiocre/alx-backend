bb3ba9a0e8f98267d4d3293ff29b4221db1c7d44
madiocre <wolvirex@gmail.com>
2024-08-02 23:09:04 +0300

    0-app.py fixes-2

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,22 +1,17 @@
-# Importing flask module in the project is mandatory
-# An object of Flask class is our WSGI application.
-from flask import Flask
+#!/usr/bin/env python3
+'''Task 0: Basic Flask app
+'''
 
-# Flask constructor takes the name of 
-# current module (__name__) as argument.
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
-# The route() function of the Flask class is a decorator, 
-# which tells the application which URL should call 
-# the associated function.
+
 @app.route('/')
-# ‘/’ URL is bound with hello_world() function.
-def hello_world():
-    return 'Hello World'
+def index():
+    '''default route'''
+    return render_template("0-index.html",)
 
-# main driver function
-if __name__ == '__main__':
 
-    # run() method of Flask class runs the application 
-    # on the local development server.
-    app.run()
+if __name__ == "__main__":
+    app.run(debug=True)
